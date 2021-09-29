62b0540a2bc3e07e5e5aae1e0bec5bce8d3a8ba2
ELTON-GE <gexin7648@hotmail.com>
2021-09-29 21:36:55 +1000

    update_404

--- utils/auth.js
+++ utils/auth.js
@@ -17,7 +17,8 @@ export const isAuth = (req, res, next) => {
   const authorization = req.headers.authorization;
   if (authorization) {
     const token = authorization.slice(7, authorization.length);
-    jwt.verify(token, process.env.JWT_SECRET || "something", (err, decode) => {
+    console.log(process.env.JWT_SECRET,"@@@")
+    jwt.verify(token, process.env.JWT_SECRET || "ss", (err, decode) => {
       if (err) {
         res.status(401).send({ message: "Invalid token" });
       } else {
